9a92dae1557ed5d814cdec0954f0d7d7f47595db
ebuka <peterdbrainy5@gmail.com>
2022-04-03 23:14:44 +0100

    Remove the default code in the app.js, create a src directory, alongside a component and utils directory within

--- App.js
+++ App.js
@@ -3,18 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+     <Text>Our reactnative blanc canvas</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create(
+
+);
