3d6969b8ad1e6bf8f3e943647d12bdeb91a96131
ebuka <peterdbrainy5@gmail.com>
2022-04-05 13:55:48 +0100

    install react-native-community eslint and prettier dependencies, create an eslintrc file and add the configuration settings

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
+import React from 'react';
+import { 
+  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Platform,
+ } from 'react-native';
+
+ const isAndroid = Platform.OS === 'android';
 
 export default function App() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-     <Text>Our reactnative blanc canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create(
-
+{}
 );
