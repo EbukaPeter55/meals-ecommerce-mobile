c577d25229321f72e386aa3e645b69b9926ad987
ebuka <peterdbrainy5@gmail.com>
2022-04-11 23:17:38 +0100

    Update the eslint configuration settings. Install react-native-paper and add the search bar

--- App.js
+++ App.js
@@ -8,15 +8,25 @@ import {
   View,
   Platform,
  } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
  const isAndroid = Platform.OS === 'android';
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const onChangeSearch = query => setSearchQuery(query);
+
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: "green" }}>
-          <Text>search</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
+         
         </View>
         <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
           <Text>list</Text>
@@ -27,6 +37,17 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create(
-{}
-);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
