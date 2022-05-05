b6982131bb2405ad2e540636c2cd840de1dcf7f0
ebuka <peterdbrainy5@gmail.com>
2022-05-05 09:15:36 +0100

    Update all the styles to make use of the them styles for consistency

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 
  const isAndroid = Platform.OS === 'android';
@@ -9,7 +11,9 @@ export default function App() {
 
   return (
     <>
+    <ThemeProvider theme={theme}>
       <RestaurantsScreen/>
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
